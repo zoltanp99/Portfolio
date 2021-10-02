a4e0d10f313e892c23850e8a611c529e925600cf
zoltanp <zoltanp1234@gmail.com>
2021-10-02 22:36:26 +0200

    - implemented rudimentary animations to the content blocks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Bio from './components/Bio/Bio';
 import ContentBlock from './components/ContentBlock/ContentBlock';
+import * as constants from './constants';
 
 function App() {
   return (
     <div className="App">
         <Bio />
-        <ContentBlock headerTitle = 'Education' content = 'lorem ipsum' />
+        <ContentBlock headerTitle = 'Education' content = {constants.loremIpsum} />
     </div>
     );
 }
--- src/components/ContentBlock/ContentBlock.js
+++ src/components/ContentBlock/ContentBlock.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import './ContentBlock.css'
+import React from 'react';
+import  './ContentBlock.css';
 
 const ContentBlock = (props) => {
+
+    const [contentEnabled, setContentEnabled] = React.useState(false);
+    
+    const handleClick = () => {
+        setContentEnabled(!contentEnabled);
+        console.log('it happened');
+        console.log(contentEnabled);
+    }
+
+
     return (
         <div className='contentBlockWrapper'>
-            <div className='header'>
+            <div className='header' onClick = {() => handleClick()}>
                 {props.headerTitle}
             </div>
-            <div className='blockHeader'>
+            <div className={contentEnabled ? 'contentAppear' : 'contentDisappear'}>
                 {props.content}
-            </div>
+            </div>  
         </div>
     )
 }
